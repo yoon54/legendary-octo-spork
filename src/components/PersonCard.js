7fe6eb308d375beb8b53e86838b58757f42f27da
Henry Yoon <henry.yoon@hotmail.com>
2020-03-05 14:22:13 -0500

    initial commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>Birthday Button for {props.firstName} {props.lastName} </button>
+    );
+}
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        })
+    }
     render() {
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
-                <h2>{lastName},{firstName}</h2>
-                <p>Age: {age}</p>
-                <p>HairColor: {hairColor}</p>
+                <h2>{lastName}, {firstName}</h2>
+                <p>Age: {age + this.state.clickCount}</p>
+                <p>Hair Color: {hairColor}</p>
+                <Button handleClick={this.handleClick} count={this.state.clickCount} firstName = {firstName} lastName = {lastName}> </Button>
             </div>
         );
     }
